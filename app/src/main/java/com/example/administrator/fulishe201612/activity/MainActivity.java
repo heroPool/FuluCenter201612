5c7e67417290b18aa5fe35925eb85472dc145900
zhuHuaJian <935625755@qq>
2017-03-14 16:28:41 +0800

    添加底部菜单栏

--- app/src/main/java/com/example/administrator/fulishe201612/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/fulishe201612/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.administrator.fulishe201612.activity;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.administrator.fulishe201612.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
