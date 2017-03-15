cce93d2e4917824f5ca24fe79570a279913bd44d
zhuHuaJian <935625755@qq>
2017-03-15 09:55:01 +0800

    新品下载接口

---
+++ app/src/main/java/com/example/administrator/fulishe201612/model/net/NewGoodsModel.java
@@ -0,0 +1,25 @@
+package com.example.administrator.fulishe201612.model.net;
+
+import android.content.Context;
+
+import com.example.administrator.fulishe201612.application.I;
+import com.example.administrator.fulishe201612.model.bean.NewGoodsBean;
+import com.example.administrator.fulishe201612.model.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+public class NewGoodsModel implements INewGoodsModel {
+    @Override
+    public void loadData(Context context, int pageId, OnCompleteListener listener) {
+        OkHttpUtils<NewGoodsBean[]> okHttpUtils = new OkHttpUtils<>(context);
+        okHttpUtils.url(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID, String.valueOf(I.CAT_ID))
+                .addParam(I.PAGE_ID, 0 + "")
+                .addParam(I.PAGE_SIZE, I.PAGE_SIZE_DEFAULT + "")
+                .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+
+    }
+}
