809771a92c86e35ba32eeae64a1d013e00c18f79
zhuHuaJian <935625755@qq>
2017-03-16 00:04:45 +0800

    完成分类页，但是子项与父项数据不匹配还未解决

---
+++ app/src/main/java/com/example/administrator/fulishe201612/adapter/ExpandableAdapter.java
@@ -0,0 +1,146 @@
+package com.example.administrator.fulishe201612.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.administrator.fulishe201612.R;
+import com.example.administrator.fulishe201612.model.bean.CategoryChildBean;
+import com.example.administrator.fulishe201612.model.bean.CategoryGroupBean;
+import com.example.administrator.fulishe201612.model.bean.NewGoodsBean;
+import com.example.administrator.fulishe201612.model.utils.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+public class ExpandableAdapter extends BaseExpandableListAdapter {
+    Context context;
+    ArrayList<CategoryGroupBean> categoryGroupBeen;
+    ArrayList<ArrayList<CategoryChildBean>> categoryChildBeen;
+
+    public ExpandableAdapter(Context context, ArrayList<CategoryGroupBean> categoryGroupBeen,
+                             ArrayList<ArrayList<CategoryChildBean>> categoryChildBeen) {
+        this.context = context;
+        this.categoryGroupBeen = categoryGroupBeen;
+        this.categoryChildBeen = categoryChildBeen;
+    }
+
+
+
+    @Override
+
+    public int getGroupCount() {
+        return categoryGroupBeen.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return categoryChildBeen.get(groupPosition).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return categoryGroupBeen.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return categoryChildBeen.get(groupPosition).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        GroupHolder holder = null;
+        if (convertView == null) {
+            holder = new GroupHolder();
+            convertView = View.inflate(context, R.layout.item_group, null);
+            holder.textGroupName = (TextView) convertView.findViewById(R.id.textGroupName);
+            holder.imageGroup = (ImageView) convertView.findViewById(R.id.imageGroup);
+            convertView.setTag(holder);
+        } else {
+            holder = (GroupHolder) convertView.getTag();
+        }
+        CategoryGroupBean bean = categoryGroupBeen.get(groupPosition);
+        holder.textGroupName.setText(bean.getName());
+        ImageLoader.downloadImg(context, holder.imageGroup, bean.getImageUrl());
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ChildeHolder holder = null;
+        if (convertView == null) {
+            holder = new ChildeHolder();
+            convertView = View.inflate(context, R.layout.item_child, null);
+            holder.textChildname = (TextView) convertView.findViewById(R.id.textChildName);
+            holder.imageChild = (ImageView) convertView.findViewById(R.id.imageChild);
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ChildeHolder) convertView.getTag();
+
+        }
+        ArrayList<CategoryChildBean> categoryChildBeen = this.categoryChildBeen.get(groupPosition);
+        CategoryChildBean bean = categoryChildBeen.get(childPosition);
+        holder.textChildname.setText(bean.getName());
+        ImageLoader.downloadImg(context, holder.imageChild, bean.getImageUrl());
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    class GroupHolder {
+
+        ImageView imageGroup;
+
+        TextView textGroupName;
+
+
+    }
+
+    class ChildeHolder {
+        ImageView imageChild;
+        TextView textChildname;
+    }
+
+    public void initContactgroup(ArrayList<CategoryGroupBean> goodsList) {
+
+        this.categoryGroupBeen.addAll(goodsList);
+        notifyDataSetChanged();
+
+    }
+
+    public void initContactchild(ArrayList<ArrayList<CategoryChildBean>> goodsList) {
+
+        this.categoryChildBeen.addAll(goodsList);
+        notifyDataSetChanged();
+
+    }
+}
--- app/src/main/java/com/example/administrator/fulishe201612/fragments/Fenlei.java
+++ app/src/main/java/com/example/administrator/fulishe201612/fragments/Fenlei.java
@@ -3,17 +3,34 @@ package com.example.administrator.fulishe201612.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 import com.example.administrator.fulishe201612.R;
+import com.example.administrator.fulishe201612.adapter.ExpandableAdapter;
+import com.example.administrator.fulishe201612.application.I;
+import com.example.administrator.fulishe201612.model.bean.CategoryChildBean;
+import com.example.administrator.fulishe201612.model.bean.CategoryGroupBean;
+import com.example.administrator.fulishe201612.model.utils.OkHttpUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Fenlei extends Fragment {
+    ExpandableListView expandableListView;
 
+    ArrayList<CategoryGroupBean> categoryGroupBeen;
+    ArrayList<ArrayList<CategoryChildBean>> categoryChildBeen;
+
+
+    ExpandableAdapter expandableAdapter;
 
     public Fenlei() {
         // Required empty public constructor
@@ -24,7 +41,77 @@ public class Fenlei extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fenlei, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_fenlei, container, false);
+
+        categoryGroupBeen = new ArrayList<>();
+        categoryChildBeen = new ArrayList<>();
+
+
+        initView(inflate);
+        initData();
+        return inflate;
+    }
+
+    private void initData() {
+
+        final OkHttpUtils<CategoryGroupBean[]> okHttpUtils = new OkHttpUtils<>(getActivity());
+        okHttpUtils.setRequestUrl(I.REQUEST_FIND_CATEGORY_GROUP)
+                .targetClass(CategoryGroupBean[].class)
+                .execute(new OkHttpUtils.OnCompleteListener<CategoryGroupBean[]>() {
+                    @Override
+                    public void onSuccess(CategoryGroupBean[] result) {
+                        ArrayList<CategoryGroupBean> list = okHttpUtils.array2List(result);
+                        expandableAdapter.initContactgroup(list);
+                        for (CategoryGroupBean l : list) {
+                            int id = l.getId();
+                            Log.i("list", "id" + id + "");
+
+                            downloadChildcate(id);
+                        }
+                        expandableAdapter.initContactchild(categoryChildBeen);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+
+                    }
+                });
+//        ArrayList<CategoryGroupBean> categoryGroupBeenL = expandableAdapter.getCategoryGroupBeen();
+//        for (int i = 0; i < categoryGroupBeenL.size(); i++) {
+//            CategoryGroupBean bean = categoryGroupBeenL.get(i);
+//            int id = bean.getId();
+//            Log.i("main", id + "");
+//            downloadChildcate(id);
+//        }
+
+        Log.i("list", "cate" + categoryChildBeen.toString());
+
+
+    }
+
+    private void downloadChildcate(final int parentId) {
+        final OkHttpUtils<CategoryChildBean[]> okHttpUtils1 = new OkHttpUtils<>(getActivity());
+        okHttpUtils1.setRequestUrl(I.REQUEST_FIND_CATEGORY_CHILDREN)
+                .addParam(I.CategoryChild.PARENT_ID, parentId + "")
+                .targetClass(CategoryChildBean[].class)
+                .execute(new OkHttpUtils.OnCompleteListener<CategoryChildBean[]>() {
+                    @Override
+                    public void onSuccess(CategoryChildBean[] result) {
+                        ArrayList<CategoryChildBean> list = okHttpUtils1.array2List(result);
+                        categoryChildBeen.add(list);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+
+                    }
+                });
+    }
+
+    private void initView(View inflate) {
+        expandableListView = (ExpandableListView) inflate.findViewById(R.id.expandableListView);
+        expandableAdapter = new ExpandableAdapter(getActivity(), categoryGroupBeen, categoryChildBeen);
+        expandableListView.setAdapter(expandableAdapter);
     }
 
 }
